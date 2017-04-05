5fc21ecd692367d26f75ea7322362491e4489c17
Michael Gulley <mgulley.mograph@gmail.com>
2017-04-05 16:17:40 -0400

    cleaning up code

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,1 @@
 angular.module("todoListApp", []);
-
-//  .controller('coolCtrl', function($scope) {
-//    $scope.whoAmI = function() {
-//      console.log('Hello there, this is the coolCtrl function!');
-//    };
-//    
-//    $scope.helloWorld = function() {
-//      console.log('This is NOT the main ctrl!');
-//    };
-//  })
-//  .controller('imASibling', function($scope) {
-//    $scope.foobar = 1234;
-//  });
-
-
-
-
